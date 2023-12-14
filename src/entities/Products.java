e9a20266582b0570c7a8146033d000d0d0c8cd14
walterdevreis <walter-reis@outlook.com>
2023-12-14 15:53:28 -0300

    terminada a classe Products

--- src/entities/Products.java
+++ src/entities/Products.java
@@ -1,4 +1,48 @@
 package src.entities;
 
 public class Products {
+
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Products(){
+    }
+
+    public Products(String name, Double price, Integer quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Double totalValue(){
+        return getQuantity() * getPrice();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s,%.2f", getName(), totalValue()));
+        return sb.toString();
+    }
 }
