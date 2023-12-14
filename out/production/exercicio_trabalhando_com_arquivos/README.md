075b70f8d68794bc9e3eb57709a81975c7e57970
walterdevreis <walter-reis@outlook.com>
2023-12-14 15:58:02 -0300

    classe Products testada e aprovada

---
+++ out/production/exercicio_trabalhando_com_arquivos/README.md
@@ -0,0 +1,1 @@
+# exercicio_trabalhando_com_arquivos
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,17 @@
 package src.application;
 
+import src.entities.Products;
+
+import java.util.Locale;
+
 public class Program {
+
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+
+        Products products = new Products("S21 Fe", 1000.00, 3);
+
+        System.out.println(products);
+    }
 }
