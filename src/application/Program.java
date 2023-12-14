f268bf87d16bc3a977c8f4a7a01404b9812a58c4
walterdevreis <walter-reis@outlook.com>
2023-12-14 17:19:32 -0300

    Projeto concluido

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,56 @@ package src.application;
 
 import src.entities.Products;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args){
 
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        Products products = new Products("S21 Fe", 1000.00, 3);
+        System.out.print("Enter the file path: ");
+        String path = sc.nextLine();
 
-        System.out.println(products);
+        List<Products> productsList = new ArrayList<>();
+
+        File sourceFile = new File(path);
+        String sourceFolder = sourceFile.getParent();
+
+        boolean sucess = new File(sourceFolder + "\\out").mkdir();
+
+        String targetFile = sourceFolder + "\\out\\summary.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            String line = br.readLine();
+            while (line != null){
+                String[] item = line.split(",");
+                String name = item[0];
+                double price = Double.parseDouble(item[1]);
+                int quantity = Integer.parseInt(item[2]);
+                productsList.add(new Products(name, price, quantity));
+
+                line = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))){
+                for(Products items : productsList){
+                    bw.write(String.format("%s,%.2f", items.getName(), items.totalValue()));
+                    bw.newLine();
+                }
+            }
+            catch (IOException e){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
